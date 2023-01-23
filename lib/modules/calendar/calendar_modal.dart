7fd0c9776420eb865e9f6a0f4ce97900cd34641e
zelenin.andrew <zelenin.andrew@gmail.com>
2023-01-23 15:03:55 +0400

    Calendar navigation

---
+++ lib/modules/calendar/calendar_modal.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarModal extends StatelessWidget {
+  const CalendarModal(
+      {super.key, required this.visibleDay, required this.onDaySelect});
+
+  final DateTime visibleDay;
+  final Function(DateTime date) onDaySelect;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TableCalendar(
+            selectedDayPredicate: ((day) => isSameDay(visibleDay, day)),
+            onDaySelected: (selectedDay, focusedDay) {
+              onDaySelect(selectedDay);
+            },
+            calendarStyle: const CalendarStyle(
+                selectedTextStyle: TextStyle(color: Colors.white),
+                selectedDecoration:
+                    BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+                todayTextStyle: TextStyle(color: Colors.black),
+                todayDecoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle)),
+            headerStyle: const HeaderStyle(
+                titleCentered: true, formatButtonVisible: false),
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: visibleDay,
+          ),
+          ElevatedButton(
+                  onPressed: () {
+                    onDaySelect(DateTime.now());
+                  },
+                  child: const Text('Today').padding(vertical: 15))
+              .padding(horizontal: 20, vertical: 10)
+        ],
+      ),
+    );
+  }
+}
--- lib/modules/days/widgets/day_app_bar.dart
+++ lib/modules/days/widgets/day_app_bar.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:smuzy_flutter/common/theme/fonts.dart';
 import 'package:smuzy_flutter/common/utils/dates.dart';
+import 'package:smuzy_flutter/modules/calendar/calendar_modal.dart';
 import 'package:smuzy_flutter/modules/days/day_provider.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class DayAppBar extends HookConsumerWidget implements PreferredSizeWidget {
   const DayAppBar({super.key});
@@ -15,7 +19,39 @@ class DayAppBar extends HookConsumerWidget implements PreferredSizeWidget {
     var visibleDay = ref.watch(dayProvider).visibleDate;
 
     return AppBar(
-      title: Text(getCurrentFormattedDay(visibleDay), style: AppFonts.title),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              showCupertinoModalBottomSheet(
+                backgroundColor: Colors.black,
+                context: context,
+                builder: (context) => CalendarModal(
+                  visibleDay: visibleDay,
+                  onDaySelect: (selectedDate) {
+                    ref
+                        .read(dayProvider.notifier)
+                        .changeVisibleDate(selectedDate);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              );
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(getCurrentFormattedDay(visibleDay), style: AppFonts.title),
+                const Icon(
+                  Ionicons.chevron_down,
+                  size: 18,
+                ).padding(left: 3)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
