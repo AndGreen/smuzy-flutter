12233bc9e39d1d4e4901bc2d30b77a869d20245d
zelenin.andrew <zelenin.andrew@gmail.com>
2023-01-27 21:51:02 +0400

    Fixed only portrait orientation on main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smuzy/modules/app/app.dart';
 import 'package:smuzy/modules/app/local_database.dart';
 
-void main() async {
+void initConfigs() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+}
+
+void main() async {
+  initConfigs();
   await LocalDatabase.init();
   runApp(const App());
 }
