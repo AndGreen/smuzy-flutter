75f0073c4c46cdb0884e0d31ce09ee012c7229c4
zelenin.andrew <zelenin.andrew@gmail.com>
2023-01-28 18:02:14 +0400

    Refactor code

--- lib/common/theme/colors.dart
+++ lib/common/theme/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// TODO: rewrite using Context extension
 class AppColors {
   static const active = Color(0xffF5FFA7);
   static const white = Color(0xffffffff);
@@ -11,25 +10,6 @@ class AppColors {
   static const lightBg = Color(0xfff2f2f7);
   static const border = Color(0xff1C1D21);
   static const borderLight = Color(0xFFC2C2C2);
-
-  static var defaultRoutinesColors = {
-    "red": const Color(0xffe25241),
-    "pink": const Color(0xffd73965),
-    "purple": const Color(0xff9037aa),
-    "deepPurple": const Color(0xff6041b1),
-    "indigo": const Color(0xff4154af),
-    "blue": const Color(0xff4696ec),
-    "cyan": const Color(0xff53bad1),
-    "teal": const Color(0xff419488),
-    "green": const Color(0xff67ac5c),
-    "lightGreen": const Color(0xffd1da59),
-    "yellow": const Color(0xfffdea60),
-    "orange": const Color(0xfff29c38),
-    "brown": const Color(0xff74564a),
-    "gray": const Color(0xff9e9e9e),
-    "blueGray": const Color(0xff667d8a),
-    "black": const Color(0xff344047),
-  };
 }
 
 extension DarkMode on BuildContext {
--- lib/common/utils/dates.dart
+++ lib/common/utils/dates.dart
@@ -1,5 +1,3 @@
-import 'package:intl/intl.dart';
-
 extension DateTimeExtensions on DateTime {
   DateTime get startOfDay => DateTime(year, month, day);
 
@@ -9,27 +7,5 @@ extension DateTimeExtensions on DateTime {
     }
 
     return year == b.year && month == b.month && day == b.day;
-  }
-
-  String getFormatted() {
-    var formattedDate = '';
-    var diffNow = startOfDay.difference(DateTime.now().startOfDay).inDays;
-    var dayOfWeek = DateFormat('E').format(DateTime.now());
-
-    switch (diffNow) {
-      case 0:
-        formattedDate = 'Today - $dayOfWeek';
-        break;
-      case 1:
-        formattedDate = 'Tomorrow - $dayOfWeek';
-        break;
-      case -1:
-        formattedDate = 'Yesterday - $dayOfWeek';
-        break;
-      default:
-        formattedDate = DateFormat('MM.dd.yy - ').format(this) + dayOfWeek;
-    }
-
-    return formattedDate.toLowerCase();
   }
 }
